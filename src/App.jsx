9cff5fe11e67a9b874a608dad686f24ebb0b728f
Barney Whiffin <barneywhiffin@gmail.com>
2026-07-03 21:27:45 +0100

    feat: initial card creation and styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 style={{ textAlign: "center", color: "purple" }}>Clade Map</h1>
+      <h1>Clade Map</h1>
       <CladeCard/>
     </>
   )
--- src/components/clade-card/clade-card.jsx
+++ src/components/clade-card/clade-card.jsx
@@ -1,7 +1,14 @@
 import './clade-card.css'
 
 function CladeCard() {
-    return <p>test</p>;
+    return (
+        <>
+            <div class='clade-card'>
+                <h3>Sarcopterygii</h3>
+                <p style={{fontStyle: "italic", marginTop: "10px"}}>'Lobe finned fish'</p>
+            </div>
+        </>
+    );
 }
 
 export default CladeCard;
