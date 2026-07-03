9abc16ff050adfd9676b2c4226fd13eb28e3d664
Barney Whiffin <barneywhiffin@gmail.com>
2026-07-03 21:55:19 +0100

    feat: add image to clade card

--- src/components/clade-card/clade-card.jsx
+++ src/components/clade-card/clade-card.jsx
@@ -1,11 +1,13 @@
 import './clade-card.css'
+import Sarcopterygii from '/src/assets/sarcopterygii.png'
 
 function CladeCard() {
     return (
         <>
-            <div class='clade-card'>
+            <div className="clade-card">
                 <h3>Sarcopterygii</h3>
                 <p style={{fontStyle: "italic", marginTop: "10px"}}>'Lobe finned fish'</p>
+                <img className="clade-img" src={Sarcopterygii} width="200px" alt="A lobe finned fish" />
             </div>
         </>
     );
