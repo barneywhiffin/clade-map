5ba86c80c08e19bb25d7a52848f60161a8d17cf7
Barney Whiffin <barneywhiffin@gmail.com>
2026-07-04 07:21:04 +0100

    feat: add display of current version

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import CladeCard from './components/clade-card/clade-card'
 import Clade from '/src/assets/data/clades.json'
+import Package from '../package.json'
 
 function App() {
 
   return (
     <>
       <h1>Clade Map</h1>
+      <p>v{Package.version}</p>
       <CladeCard title={Clade.title} description={Clade.description} img={Clade.img} />
     </>
   )
